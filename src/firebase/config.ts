5b6869ea40235a598eeb20be47bdca872ef2de03
Ringuyeneza Salton <nezasalton@gmail.com>
2026-02-18 20:36:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6020157995-91814",
+  "appId": "1:94436231940:web:82f5a9b26165590ee39e51",
+  "apiKey": "AIzaSyB_4qZId3m3pVKfKxGnl5D27E9XhTLPJiw",
+  "authDomain": "studio-6020157995-91814.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "94436231940"
+};
